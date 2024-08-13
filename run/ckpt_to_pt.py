913719d0212b045e399f14ec1c62c63bf34af654
Mikhail Kotyushev <mkotyushev@gmail.com>
2024-08-13 19:00:26 +0000

    Add weights conversion script

---
+++ run/ckpt_to_pt.py
@@ -0,0 +1,24 @@
+import argparse
+import torch
+from pathlib import Path
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cpkt_filepaths', type=Path, nargs='+', help='Path(s) to checkpoint files')
+    parser.add_argument('output_dirpath', type=Path, help='Path to the directory to save the pytorch model files')
+    return parser.parse_args()
+
+
+def main(args):
+    for cpkt_filepath in args.cpkt_filepaths:
+        output_filepath = args.output_dirpath / cpkt_filepath.with_suffix('.pt').name
+        print(f'Converting {cpkt_filepath} to {output_filepath}')
+        ckpt = torch.load(cpkt_filepath)
+        state_dict = ckpt['state_dict']
+        torch.save(state_dict, output_filepath)
+    
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
